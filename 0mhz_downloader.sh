78cd109e9cc8164231b2e76f93110aabb4b1b90a
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2024-03-31 17:15:35 -0700

    Update 0mhz_downloader.sh

--- 0mhz_downloader.sh
+++ 0mhz_downloader.sh
@@ -20,13 +20,19 @@
 # Where should the games be installed? Change accordingly if you want games to be stored on usb
 games_loc="/media/fat"
 
+# Path for mgl files. Should be on /media/fat drive.
+dos_mgl="/media/fat/_DOS Games"
+
+#Cleanup files in /media directory. If a file 
+delete_unmatched_files=false
+
+# Always download fresh copies of mgl's to assure we stay up to date
+always_dl_mgl=false
+
 
 ###### The rest of the script should probably not be changed 
 
 base_dir="${games_loc}/games/AO486"
-
-# Path for mgl files. Always on SD card drive
-dos_mgl="/media/fat/_DOS Games"
 
 # URL of the 0mhz archive.org XML file
 xml_url="https://archive.org/download/0mhz-dos/0mhz-dos_files.xml"
@@ -37,6 +43,12 @@ api_url="https://api.github.com/repos/0mhz-net/0mhz-collection/contents/mgls?ref
 # Ensure the local directory exists
 mkdir -p "$dos_mgl"
 mkdir -p "$base_dir"
+
+# Empty out the mgl_dir if always_dl_mgl is true
+if [ "$always_dl_mgl" = true ]; then
+    echo "Clearing out $mgl_dir..."
+    rm -f "$dos_mgl"/*.mgl
+fi
 
 # Check available space on the device where base_dir is located
 available_space_gb=$(df -BG "$base_dir" | awk 'NR==2 {print substr($4, 1, length($4)-1)}')
@@ -54,7 +66,7 @@ curl --insecure -s "$api_url" | jq -r '.[] | select(.type=="file") | .download_u
     file_name=$(basename "$file_url" | perl -pe 's/%([0-9A-F]{2})/chr(hex($1))/eg')
 
     # Check if the file already exists locally
-    if [ ! -f "$file_name" ]; then
+    if [ "$always_dl_mgl" = true ] || [ ! -f "$file_name" ]; then
         echo "Downloading $file_name..."
         curl --insecure -s -o "$file_name" "$file_url"
     else
@@ -67,8 +79,12 @@ echo "Checking if files exist"
 
 mgl_dir="$dos_mgl"
 
+# Initialize an array to hold all paths mentioned in mgl
+referenced_paths=()
+
 # Initialize an array to hold the names of .mgl files with missing paths/files
 mgl_with_missing_paths=()
+
 
 # Loop through all .mgl files in the mgl_dir
 for mgl_file in "$mgl_dir"/*.mgl; do
@@ -87,8 +103,9 @@ for mgl_file in "$mgl_dir"/*.mgl; do
         trimmed_path=$(echo "$path" | sed 's/^ *//;s/ *$//')
 
         full_path="${base_dir}/${trimmed_path}"
-
-        echo "Checking existence of: $full_path"
+        referenced_paths+=("$full_path")
+		
+        echo "Checking: $full_path"
 
         if [ ! -f "$full_path" ]; then
             echo "Missing: $trimmed_path"
@@ -107,7 +124,7 @@ done
 if [ ${#mgl_with_missing_paths[@]} -eq 0 ]; then
     echo "No .mgl files with missing paths were found."
 else
-    echo ""
+	echo ""
     echo "List of .mgl files with missing files:"
     for mgl_file in "${mgl_with_missing_paths[@]}"; do
         echo "$mgl_file"
@@ -125,13 +142,13 @@ for mgl_file in "${mgl_with_missing_paths[@]}"; do
 
     # Check if the .zip version exists in the file names
     if echo "$file_names" | fgrep -qi "$zip_name"; then
-        echo ""
+		echo ""
         echo "Downloading missing zip: $zip_name"
         # Print the download link for the .zip file
         dl_zip="$(echo https://archive.org/download/0mhz-dos/"$zip_name" | sed 's/ /%20/g')"
 		mkdir -p ${base_dir}/.0mhz_downloader
 		curl --insecure -L -# -o "${base_dir}/.0mhz_downloader/$zip_name" "$dl_zip"		
-		if unzip -o "$base_dir/.0mhz_downloader/${zip_name}" -d "$games_loc"; then
+		if unzip -o "$base_dir/.0mhz_downloader/${zip_name}" "media/*" -d "$games_loc"; then
 			echo "Unzipped $zip_name successfully."
 			rm "$base_dir/.0mhz_downloader/${zip_name}"
 		else
@@ -139,3 +156,21 @@ for mgl_file in "${mgl_with_missing_paths[@]}"; do
 		fi		
     fi
 done
+
+if [ "$delete_unmatched_files" = true ]; then
+    echo "File cleanup is enabled."
+	media_dir="${base_dir}/media"
+
+	# List all files in the media directory
+	find "$media_dir" -type f | while read media_file; do
+		
+		# Check if the relative path is in the referenced_paths array
+		if [[ ! " ${referenced_paths[@]} " =~ " ${media_file} " ]]; then
+			echo "Deleting file: $media_file"
+			# Prompt for deletion or directly delete as per your requirement
+			rm "$media_file"
+		fi
+	done
+else
+    echo "Skipping deletion of unreferenced files."
+fi
