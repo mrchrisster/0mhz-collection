d072a1773b93441a538aa65e26533a366aedaea5
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2024-04-02 17:03:09 -0700

    Update 0mhz_downloader.sh

--- 0mhz_downloader.sh
+++ 0mhz_downloader.sh
@@ -19,7 +19,7 @@
 # NOTE: If this is the first time you install ao486, make sure you run update_all to install all necessary files for the core
 
 # Where should the games be installed? Change accordingly if you want games to be stored on usb or cifs
-games_loc="/media/usb0"
+games_loc="/media/fat"
 
 # Path for mgl files. Should be on /media/fat drive.
 dos_mgl="/media/fat/_DOS Games"
