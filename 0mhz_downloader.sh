e92cc8858a92fce133348ccaf129b80c2120d733
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2024-04-19 23:41:10 -0700

    Update 0mhz_downloader.sh

--- 0mhz_downloader.sh
+++ 0mhz_downloader.sh
@@ -37,7 +37,7 @@ dos_mgl="/media/fat/_DOS Games"
 # Prefer mt32 files. This will download all mgl files but if a MT-32 version exist, it will use that version.
 prefer_mt32=false
 
-# Always download fresh copies of mgls to stay up to date. CAUTION: Deletes mgl folder and any custom mgls.
+# Always download fresh copies of mgls to stay up to date. CAUTION: Deletes any custom mgls you may have created.
 always_dl_mgl=false
 
 # Deletes mgls that are not associated with files on archive. Set to false to disable automatic deletion
